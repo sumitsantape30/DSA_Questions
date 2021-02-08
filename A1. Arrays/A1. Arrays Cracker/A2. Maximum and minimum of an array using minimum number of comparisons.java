4220e5e74b261c947f8fb568ccab902e33874d4e
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-08 22:26:49 +0530

    Update A2. Maximum and minimum of an array using minimum number of comparisons.java

--- A1. Arrays/A1. Arrays Cracker/A2. Maximum and minimum of an array using minimum number of comparisons.java
+++ A1. Arrays/A1. Arrays Cracker/A2. Maximum and minimum of an array using minimum number of comparisons.java
@@ -49,7 +49,7 @@ public class minAndmaxOfArray {
 		}
 		
 		//iske bad hum apna loop run karenge fron 2th index to end and will compare al the elements with out min and max agar woh first two elements se minimim or maximum hai to hum apne min and max ko updata kar denge
-		for( int i=2; i< n; i++) {
+		for( int i=2; i< n; i++) { //loop is running for n-2, and for every interation we have two operations there so in this problem total comparisons are 2*(n-2) + 1
 			
 			if( arr[i] > minmax.max) {
 				minmax.max= arr[i];
@@ -79,4 +79,92 @@ Output:
 
 Time Complexity: O(n)
 
-//=================================
+//================================= Approach 3=================================================
+// agar even number of elements hai to first two elements ke min and max ko as minimum and maximum initialise karenge.
+//agar odd number of elements hai to initialse min and max as first element, and baki elements ko pick them in pairs and compare their maximum and minimum with max and min respectively. 
+// even number of elements hai aur hum pair mai pick karke compare karre isliye first two ko min max assume kiya taki last mai koi element bach na jaye. Aur odd number of elements hai to sirf first element ko min max assume karre taki bache hue elements mai pair wise compare ho ske.
+	
+	package ArraysCracker;
+import java.util.Scanner;
+
+class Pair{
+	int min;
+	int max;
+}
+
+public class minAndmaxOfArray2 {
+	
+	public static int[] takeInput() {
+		
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		int arr[]= new int[n];
+		
+		for( int i=0; i< arr.length; i++) {
+			arr[i]= s.nextInt();
+		}
+		return arr;
+	}
+	
+	public static Pair getminmax( int arr[]) {
+		
+		Pair minmax= new Pair();
+		
+		int i = 0; //i rakh rhe, agar mere array mai even elements hai to mai array ko do elements chhodke start karunga else odd hai to mai array ko ek element chhodke start karunga
+		int n= arr.length;
+		
+		if( n % 2 ==0) { //agar mere array mai even number of elements hai to mai first two elements ko min max se initialize karunga aur mera i 2 se increment hoga coz mera array even hai aur hum niche pair wise compare karre to puri tarah se compare hona chahiye isliye
+			if( arr[0] < arr[1]) {
+				minmax.min= arr[0];
+				minmax.max= arr[1];
+			}else {
+				minmax.min= arr[1];
+				minmax.max= arr[0];
+			}
+			 i +=2; //setting the starting index for the loop
+		}else { //agar mere array mai odd elements hai to mai first element kohi min and max manunga
+			
+			minmax.min= arr[0];
+			minmax.max= arr[0];
+			
+			i=1; //setting the starting index for the loop
+		}
+		
+		//ab loop mai do do elements ki pair pick karnge aur min and max ke sath compare karenge
+		while( i < n-1) {
+			
+			if( arr[i]> arr[i+1]) {
+				
+				if( arr[i]> minmax.max) {
+					minmax.max= arr[i];
+				}
+				
+				if( arr[i+1] < minmax.min) {
+					minmax.min= arr[i+1];
+				}
+			}else {
+				
+				if( arr[i+1] > minmax.max) {
+					minmax.max= arr[i+1];
+				}
+				
+				if(arr[i] < minmax.min) {
+					minmax.min= arr[i];
+				}
+				
+			}
+			i +=2; //i ko 2 se increment karenge as hum do do elements ko compare karre	
+		}
+		return minmax;
+	}
+
+	public static void main(String[] args) {
+
+		int arr[]= takeInput();
+		Pair minmax= getminmax(arr);
+		
+		System.out.println(minmax.min);
+		System.out.println(minmax.max);
+	}
+
+}
