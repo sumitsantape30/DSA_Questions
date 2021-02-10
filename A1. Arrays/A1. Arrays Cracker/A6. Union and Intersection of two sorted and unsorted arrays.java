a35ccd05ab56c25bedb036afb0c58a71e2804fc8
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-10 09:47:07 +0530

    Update and rename A6. Union and Intersection of two unsorted arrays.java to A6. Union and Intersection of two sorted and unsorted arrays.java

--- A1. Arrays/A1. Arrays Cracker/A6. Union and Intersection of two sorted and unsorted arrays.java
+++ A1. Arrays/A1. Arrays Cracker/A6. Union and Intersection of two sorted and unsorted arrays.java
@@ -1,3 +1,31 @@
+Union and Intersection of two sorted arrays
+
+Given two sorted arrays, find their union and intersection.
+Example:
+
+Input : arr1[] = {1, 3, 4, 5, 7}
+        arr2[] = {2, 3, 5, 6} 
+Output : Union : {1, 2, 3, 4, 5, 6, 7} 
+         Intersection : {3, 5}
+
+Input : arr1[] = {2, 5, 6}
+        arr2[] = {4, 6, 8, 10} 
+Output : Union : {2, 4, 5, 6, 8, 10} 
+         Intersection : {6}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Given two arrays A and B of size N and M respectively. The task is to find union between these two arrays.
 
 Union of the two arrays can be defined as the set containing distinct elements from both the arrays. If there are repetitions, then only one occurrence of element should be printed in union.
@@ -29,3 +57,4 @@ union set of both arrays. So count is 7.
 
 //=======================================Approach 1  (Using map data structure)==========================================
 Code:
+
