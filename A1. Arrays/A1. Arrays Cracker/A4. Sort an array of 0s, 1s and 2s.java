85190f43c58de39be23271ff567d164defb60a1f
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-12 10:36:45 +0530

    Update A4. Sort an array of 0s, 1s and 2s.java

--- A1. Arrays/A1. Arrays Cracker/A4. Sort an array of 0s, 1s and 2s.java
+++ A1. Arrays/A1. Arrays Cracker/A4. Sort an array of 0s, 1s and 2s.java
@@ -14,6 +14,99 @@ Code:
 We can simply sort the array using a sorting algorithm. It will take O(logn) time for sorting and O(nlogn) time for sorting n elements, and O(1) space.
 
 //====================================Approach 2 (Simple Counting)================================
+Count the number of 0s, 1s and 2s in the given array. Then store all the 0s in the beginning followed by all the 1s then all the 2s.
+
+Code:
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static int[] takeInput() {
+		
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		int arr[]= new int[n];
+		
+		for( int i=0; i< arr.length; i++) {
+			arr[i]= s.nextInt();
+		}
+		return arr;
+	}
+
+	public static void sort(int arr[]) {
+
+		int n = arr.length;
+		// hum 3 count variables rakhenge jisme count store hoga of number of 0s 1s and 2s
+		int cnt0 = 0;
+		int cnt1 = 0;
+		int cnt2 = 0;
+		
+        // jo bhi change karenge woh ek hi array mai karenge isliye ek hi main wala loop rahega
+		// now we'll linearly traverse in the array and count the number of 0s, 1s and 2s
+		for (int i = 0; i < arr.length; i++) {
+
+			switch (arr[i]) {
+
+			case 0: {
+				cnt0++;
+				break;
+			}
+
+			case 1: {
+				cnt1++;
+				break;
+			}
+
+			case 2: {
+				cnt2++;
+				break;
+			}
+		}
+		
+		// ab mere jintna count hoga of each integer utne times woh integer array mai dalenge.
+
+		  i = 0; // 3 bar loop hoga so and we have to add elements in sme array isliye teeno keliye ek hi i rahega
+
+		while (cnt0 > 0) {
+			arr[i++] = 0;
+			cnt0--;
+		}
+
+		while (cnt1 > 0) {
+			arr[i++] = 1;
+			cnt1--;
+		}
+
+		while (cnt2 > 0) {
+			arr[i++] = 2;
+			cnt2--;
+		}
+	}
+
+		print(arr);
+
+	}
+
+	public static void print(int arr[]) {
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int arr[] = takeInput();
+		sort(arr);
+	}
+}
+
+	
+Complexity Analysis:
+Time Complexity: O(n).
+Only two traversals of the array is needed.
+Space Complexity: O(1).
+As no extra space is required.	
 	
 //===================================== Approach 3 (Variation of dutch national flag alorithm) =============================
 package ArraysCracker;
