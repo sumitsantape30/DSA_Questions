f4d55a5c46e3161852adf2125a4b56d16ebd6f08
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-14 09:54:07 +0530

    Update B16. Count Inversions.java

--- A1. Arrays/A1. Arrays Cracker/B16. Count Inversions.java
+++ A1. Arrays/A1. Arrays Cracker/B16. Count Inversions.java
@@ -22,3 +22,87 @@ Input: N = 3, arr[] = {10, 10, 10}
 Output: 0
 Explanation: As all the elements of array 
 are same, so there is no inversion count.
+ 
+ Code:
+
+//============================================================= Approach 1========================================
+
+//===========================================================Approach 2===========================================
+package ArraysCracker;
+
+public class CountInversions {
+	
+	public static int merge(int arr[], int sI, int eI) {
+		
+		int mid= (sI + eI)/2;
+		int ans[]= new int[ eI - sI +1];
+		int i= sI;
+		int j= mid+1;
+		int k=0;
+		int invCount=0;
+		
+		while( i <=  mid && j <= eI) {
+			
+			if( arr[i] <= arr[j]) {
+				ans[k]= arr[i];
+				i++;
+				k++;
+			}else {
+				ans[k]= arr[j];
+				j++;
+				k++;
+				//the addition will be the count of elements to right of the mid so I know that second array starts at mid+1 so from i I need to count the elements on the right
+				invCount += invCount + (mid -i);
+			}
+		}
+		
+		while( i <= mid) {
+			arr[k]= arr[i];
+			i++;
+			k++;
+		}
+		
+		while( j <= eI) {
+			arr[k]= arr[j];
+			k++;
+			j++;
+		}
+		
+		for( int index=0; index < ans.length; index++) {
+			arr[ sI+ index]= ans[index];
+		}
+		return invCount;
+		
+	}
+	
+	//here, low has been called left and high has been called right
+	public static int mergeSort(int arr[], int sI, int eI) {
+		
+		 int invCount=0; //initially count will be zero
+		
+		 //after that we know we're gonna split them only if right is greater than left 
+		if( sI > eI) {
+			
+			int mid= (sI + eI)/2;
+			
+			//splitting
+			invCount += mergeSort(arr, sI, mid);
+			invCount += mergeSort(arr, mid+1, eI);
+			
+			//after splitting the recusrsion is over we simply call the merge function
+			invCount += merge(arr, sI, eI);
+		}
+		return invCount;
+	}
+
+	public static void main(String[] args) {
+
+		int arr[]= {5,3,2,4,1};
+		int n= arr.length;
+		int ans= mergeSort(arr, 0, arr.length-1); //here, low is 0 and high will always be arr.length-1
+		System.out.println("Number of inversions"+ ans);
+		
+	}
+
+}
+
