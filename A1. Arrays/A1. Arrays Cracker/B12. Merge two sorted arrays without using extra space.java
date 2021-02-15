f30dfb62cc81ca4a8918e5634d5b256ffc090b0b
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-15 09:26:12 +0530

    Update B12. Merge two sorted arrays without using extra space.java

--- A1. Arrays/A1. Arrays Cracker/B12. Merge two sorted arrays without using extra space.java
+++ A1. Arrays/A1. Arrays Cracker/B12. Merge two sorted arrays without using extra space.java
@@ -13,8 +13,78 @@ Output: ar1[] = {1, 2, 3, 5, 8, 9}
         ar2[] = {10, 13, 15, 20}
         
 //===========================================Approach 1================================================================================================
+we are going to use the third array which will have a sie of addition of both the arrays
+Code:
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Fibonacci {
+
+	public static int[] takeInput() {
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		int arr[]= new int[n];
+		
+		for( int i=0; i< arr.length; i++) {
+			arr[i]= s.nextInt();
+		}
+		return arr;
+	}
+	
+	public static void mergeSorted(int arr1[], int arr2[]) {
+		
+		int arr3[]= new int[arr1.length+ arr2.length];
+		int index=0;
+		
+		for( int i=0; i< arr1.length; i++) {
+			arr3[index]= arr1[i];
+			index++;
+		}
+		
+		for( int i=0; i< arr2.length; i++) {
+			arr3[index]= arr2[i];
+			index++;
+		}
+		
+		Arrays.sort(arr3);
+		index=0;
+		
+		for( int i=0 ; i< arr1.length; i++) {
+			arr1[i] = arr3[index];
+			index++;
+		}
+		
+		for( int i=0 ; i< arr2.length; i++) {
+			arr2[i] = arr3[index];
+			index++;
+		}
+		
+		print(arr1);
+		print(arr2);
+	}
+	
+	public static void print( int arr[]) {
+		
+		for( int i=0; i< arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+
+		int arr1[] = takeInput();
+		int arr2[]= takeInput();
+		 mergeSorted(arr1, arr2);
+	}
+}
+
+Time Complexity: O(nlogn) + O(n) + O(n)
+Space: O(n)
 
 //============================================Approach 2 (Insertion Sort kind of algorithm)===============================================================
+       
 
 //============================================ Approach 3 =============================================================================================
 
