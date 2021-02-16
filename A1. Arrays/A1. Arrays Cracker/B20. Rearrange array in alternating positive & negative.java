0236e84c2805d9126c3e2de45ae3f40f98657eba
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-16 17:23:08 +0530

    Update B20. Rearrange array in alternating positive & negative.java

--- A1. Arrays/A1. Arrays Cracker/B20. Rearrange array in alternating positive & negative.java
+++ A1. Arrays/A1. Arrays Cracker/B20. Rearrange array in alternating positive & negative.java
@@ -10,3 +10,90 @@ Output: arr[] = {-4, 1, -1, 2, 3, 4}
 
 Input:  arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8}
 output: arr[] = {-5, 5, -2, 2, -8, 4, 7, 1, 8, 0} 
+
+//========================================================== Aproach 1===============================
+//========================================================= Approach 2====================================
+//==========================================================Approach 3==========================
+
+Code:
+
+package ArraysCracker;
+
+import java.util.Scanner;
+
+public class AlternateNegPos {
+	
+	public static int[] takeInput() {
+		
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		int arr[]= new int[n];
+		
+		for( int i=0 ;i < arr.length; i--) {
+			arr[i]= s.nextInt();
+		}
+		return arr;
+	}
+	
+	public static void rightRotate(int arr[], int from, int to) {
+		
+		int temp= arr[to];
+		
+		for( int i= to; from >0; i++) {
+			arr[i]= arr[i -1];
+		}
+		arr[from]= temp;
+	}
+	
+	public static void rearrange(int arr[]) {
+		int n= arr.length;
+		int wrongIndex = -1; // wrong index ko -1 man liye
+		
+		//ab hum iterate karenge
+		for( int i=0; i< n; i++) {
+			
+			if( wrongIndex != -1) { //agar wrong index negative nhi hai means muje kahipe wrong index mil chuki hai, agar wrong index negative hi hai means abhitak mko array mai koi wrong index nhi mili hai
+				
+				//so agar mera wrong index nhi hai 
+				// to mai check karunga ki jo wrong index pe value hai woh positive hai aur basically mai sign check kr rha hu ki opposite hai kya
+				if(( arr[wrongIndex] >= 0 && arr[i]<0 ) || (arr[wrongIndex] <= 0 && arr[i]> 0)){
+					//to mai right rotate karunga front wrong index to that particular index i
+					rightRotate(arr, wrongIndex, i);
+					
+					//right rotation ke bad mai check karunga ki uss index jis index ki mi bat kr rha tha aur jo meri wrong index hai unn dono ke bich ka difference matlab unn dono ke bich 2 elements hai ya 2 se jada elements hai
+					//agar do se jada element hai to bich mai kahi na kahi dubara ek wrong index aayi hogi to usko +2 kar dunga
+					if( i - wrongIndex >= 2) {
+						wrongIndex += 2;
+					}else { //agar 2 se kam element hai to wrongIndex ko -1 kar dunga matlab mere pas ab koi bhi wrong index nhi hai
+						
+					}
+					
+				}
+				
+			}else {  // agar wrong index negative hi hai means abhitak mko array mai koi wrong index nhi mili hai
+			
+				//so jo meri element hai woh negative hai aur sath sath mai meri index odd hai, ya fir yeh index positive hai aur sath mai hi yeh index even hai, so inn dono mese koi bhi condtion hoti hai to wahi mera wrong index hai
+				if(( arr[i] <0 && i% 2 != 0) || (arr[i] > 0 && i%2 == 0)){
+					wrongIndex = i;
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	public static void print(int arr[]) {
+		
+		for( int i=0; i< arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int arr[]= takeInput();
+		rearrange(arr);
+		print(arr);
+	}
+}
