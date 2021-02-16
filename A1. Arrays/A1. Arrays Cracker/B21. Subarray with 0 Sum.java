03653174abece99f4d50d80db8059507cf8d89fd
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-16 18:49:15 +0530

    Create B21. Subarray with 0 Sum.java

---
+++ A1. Arrays/A1. Arrays Cracker/B21. Subarray with 0 Sum.java
@@ -0,0 +1,20 @@
+Find if there is a subarray with 0 sum
+
+Given an array of positive and negative numbers, find if there is a subarray (of size at-least one) with 0 sum.
+
+Examples : 
+
+Input: {4, 2, -3, 1, 6}
+Output: true 
+Explanation:
+There is a subarray with zero sum from index 1 to 3.
+
+Input: {4, 2, 0, 1, 6}
+Output: true 
+Explanation:
+There is a subarray with zero sum from index 2 to 2.
+
+Input: {-3, 2, 3, 1, 6}
+Output: false
+
+Code:
