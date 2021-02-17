1396bbc7a90fc4ea2af0284ee89130de56b29bfd
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-17 22:27:38 +0530

    Create C25. Elements that appear more than nbyk times.java

---
+++ A1. Arrays/A1. Arrays Cracker/C25. Elements that appear more than nbyk times.java
@@ -0,0 +1,4 @@
+Given an array of size n and a number k, find all elements that appear more than n/k times
+Given an array of size n, find all elements in array that appear more than n/k times. 
+For example, if the input arrays is {3, 1, 2, 2, 1, 2, 3, 3} and k is 4, then the output should be [2, 3]. Note that size of array is 8 (or n = 8), 
+so we need to find all elements that appear more than 2 (or 8/4) times. There are two elements that appear more than two times, 2 and 3. 
