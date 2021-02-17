db9b9000524eea4df67da22d3ab2db743a9514bc
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-17 22:20:20 +0530

    Update C24. Longest consecutive subsequence.java

--- A1. Arrays/A1. Arrays Cracker/C24. Longest consecutive subsequence.java
+++ A1. Arrays/A1. Arrays Cracker/C24. Longest consecutive subsequence.java
@@ -17,3 +17,71 @@ The subsequence 36, 35, 33, 34, 32 is the longest
 subsequence of consecutive elements.
 
 Code:
+
+package ArraysCracker;
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class LongestConsecutiveSequeunce {
+	
+	public static int[] takeInput() {
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		int arr[]= new int[n];
+		
+		for( int i=0; i< arr.length; i++) {
+			arr[i]= s.nextInt();
+		}
+		return arr;
+	}
+	
+	public static void sequence(int arr[]) {
+		
+		//sabse pehle hum hashmap declare karenge integer vs boolean ka, yeh boolean yeh represent karta hai ki yaha se sequence start hota hai ya nhi aur hum shuruwat mai true dalne wale hai
+		HashMap<Integer, Boolean> map= new HashMap<Integer, Boolean>();
+		
+		//hum initially sabsko true karre ki ha tum bhi seqeunce ki shuruwat ho but yeh sach bta nhi hai
+		for( int val: arr) {
+			map.put(val, true);
+		}
+		
+		//ab lagayenge dusra loop ab ki agar map mai values se ek chota pda hai to woh value start of sequence nhi hot skta so usko false kardenge, isse sirf woh log true reh jayenge jinse chota element nhi hai to woh start of sequence ban jayenge. Wahi true rahenge baki sare false hojayenge
+		for( int val: arr) {
+			map.put(val, false);
+		}
+		
+		//ab lagayenge teesra loop isme hum unhi keliye kam karenge jinki value true hai matlab jo start of sequence hai unke liye hi kam karenge baki ke liye kam nhi karenge
+		int msp=0;
+		int ml=0;
+		for( int val: arr) {
+			if( map.get(val)) {
+				int tl= 1; //temporary length;
+				int tsp= val; //temporary start point will be this val only
+				//maine do variables banaye ki abhi jo temporary sequence hai woh val se start hoga and uski length 1 hai
+				
+				while(map.containsKey(tsp+ tl)) { //jabtak yeh milta rahega means jabtak ek val ke next wala jabtak milta rahega to temporary length ko badhate rahenge means sequence ko badhate rahenge
+					tl++;	
+				} //jis waqt nhi milega us waqt sequence bahar aajayega
+				
+				//agar meri yeh temporary length max length ko hara deti hai to max start point updata hojayege
+				if( tl > ml) {
+					msp= tsp; // max start point mai aajyega temporary start point
+					ml= tl; // and max length mai ajayega temporary length
+					
+				}
+			}
+		}
+		
+		//loop se bahar aakar ab humare pas max start point aur maximum length wali sequence hai ab usko print kardenge
+		for( int i =0; i< ml; i++) {
+			System.out.println(msp+ i);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int arr[]= takeInput();
+		sequence(arr);
+	}
+}
