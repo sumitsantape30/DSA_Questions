62bf3ded2d4eff6295ccd58e3ca8e917f506b479
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-19 13:09:53 +0530

    Update C27. Triplet Sum in an Array.java

--- A1. Arrays/A1. Arrays Cracker/C27. Triplet Sum in an Array.java
+++ A1. Arrays/A1. Arrays Cracker/C27. Triplet Sum in an Array.java
@@ -12,3 +12,79 @@ Explanation: There is a triplet (5, 3 and 1) present
 in the array whose sum is 9. 
 
 Code:
+
+package ArraysCracker;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class TripletThatSumsToGivenValue {
+	
+	public static int[] takeInput() {
+		Scanner s= new Scanner(System.in);
+		int n =s.nextInt();
+		
+		int arr[]= new int[n];
+		
+		for( int i=0; i< arr.length ; i++) {
+			arr[i]= s.nextInt();
+		}
+		return arr;
+	}
+	
+	public static void findNumbers(int arr[], int sum){
+		
+		int n= arr.length;
+		
+		for( int i=0; i< n-2; i++) {
+			
+			for( int j= i +1; j< n-1; j++) {
+				
+				for( int k= j+1; k < n; k++) {
+					
+					if( arr[i]+ arr[j]+ arr[k] == sum) {
+						System.out.println("The triplet is"+ arr[i]+" "+arr[j]+" "+arr[k]);
+
+					}
+				}
+			}
+		}
+	}
+	
+	public static boolean find3Numbers(int arr[], int sum) {
+		
+		for( int i=0; i< arr.length-2; i++) {
+			
+			int j= i+1;
+			int k= arr.length-1;
+			
+			while(j < k) {
+				
+				if(arr[i]+ arr[j]+ arr[k]== sum) {
+					System.out.println("Triplet is "+ arr[i]+" "+arr[j]+" "+arr[k]);
+					return true;
+				}
+				
+				if( arr[i]+ arr[j]+ arr[k] < sum) {
+					j++;
+				}else {
+					k--;
+				}
+			}
+		}
+		return false;
+		
+	}
+
+	public static void main(String[] args) {
+
+		int arr[]= takeInput();
+		int n= arr.length;
+		findNumbers(arr, 24);
+		boolean num= find3Numbers(arr, 24);
+		System.out.println(num);
+		
+	}
+
+}
+
