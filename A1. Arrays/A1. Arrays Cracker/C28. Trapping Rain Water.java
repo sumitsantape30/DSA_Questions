11cd7c4d7bcbfccf406001d0946cebebee187414
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-19 19:25:29 +0530

    Create C28. Trapping Rain Water.java

---
+++ A1. Arrays/A1. Arrays Cracker/C28. Trapping Rain Water.java
@@ -0,0 +1,10 @@
+Given an array arr[] of N non-negative integers representing the height of blocks. If width of each block is 1, compute how much water can be trapped between the blocks during the rainy season. 
+ 
+
+Example 1:
+
+Input:
+N = 6
+arr[] = {3,0,0,2,0,4}
+Output:
+10
