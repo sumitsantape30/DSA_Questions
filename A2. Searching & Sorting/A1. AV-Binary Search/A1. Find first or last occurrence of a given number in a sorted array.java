5459ee939edd569c8d892bf4ba8a366b40b2845f
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-21 17:10:02 +0530

    Create A1. Find first or last occurrence of a given number in a sorted array.java

---
+++ A2. Searching & Sorting/A1. AV-Binary Search/A1. Find first or last occurrence of a given number in a sorted array.java
@@ -0,0 +1,19 @@
+Find first or last occurrence of a given number in a sorted array
+
+
+Input:
+arr = [2, 5, 5, 5, 6, 6, 8, 9, 9, 9]
+target = 5
+ 
+Output:
+The first occurrence of element 5 is located at index 1
+The last occurrence of element 5 is located at index 3
+ 
+ 
+Input:
+arr = [2, 5, 5, 5, 6, 6, 8, 9, 9, 9]
+target = 4
+ 
+Output:
+Element not found in the array
+
