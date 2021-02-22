0cbb49ac4965a6c05bed24127119071823874a49
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-22 23:45:03 +0530

    Create A2. Floor of an element in a Sorted Array.java

---
+++ A2. Searching & Sorting/A1. AV- Searching&Sorting/A2. Floor of an element in a Sorted Array.java
@@ -0,0 +1,7 @@
+Given a sorted array and a value x, the floor of x is the largest element in array smaller than or equal to x. Write efficient functions to find floor of x.
+
+Examples:
+Input : arr[] = {1, 2, 8, 10, 10, 12, 19}, x = 5
+Output : 2
+2 is the largest element in 
+arr[] smaller than 5.
