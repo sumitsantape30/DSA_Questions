635f48eccb68b9dfee1a931d22d52f1fe3e26471
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-24 22:30:55 +0530

    Create A10. four elements that sum to a given value.java

---
+++ A2. Searching & Sorting/A2. Cracker/A10. four elements that sum to a given value.java
@@ -0,0 +1,8 @@
+Find four elements that sum to a given value | Set 1 (n^3 solution)
+
+Given an array of integers, find all combination of four elements in the array whose sum is equal to a given value X.
+For example, if the given array is {10, 2, 3, 4, 5, 9, 7, 8} and X = 23, then your function should print “3 5 7 8” (3 + 5 + 7 + 8 = 23).
+
+Code:
+
+
