2fede2f6e71bc98af96576a1ca0600a6ce1578d9
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-24 22:43:16 +0530

    Update B10. four elements that sum to a given value.java

--- A2. Searching & Sorting/A2. Cracker/B10. four elements that sum to a given value.java
+++ A2. Searching & Sorting/A2. Cracker/B10. four elements that sum to a given value.java
@@ -5,4 +5,36 @@ For example, if the given array is {10, 2, 3, 4, 5, 9, 7, 8} and X = 23, then yo
 
 Code:
 
+//================================================ Approach 1============================================================================
+
+package CrackerSandS;
+
+public class FourSumNumbers {
+
+	public static void forSumNum(int arr[], int x) {
+
+		int n = arr.length;
+
+		for (int i = 0; i < n - 3; i++) {
+			for (int j = i + 1; j < n - 2; j++) {
+				for (int k = j + 1; k < n - 1; k++) {
+					for (int l = k + 1; l < n; l++) {
+						if (arr[i] + arr[j] + arr[k] + arr[l] == x) {
+							System.out.println(arr[i] + " " + arr[j] + " " + arr[k] + " " + arr[l]);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int arr[] = { 10, 20, 30, 40, 1, 2 };
+		int x = 91;
+		forSumNum(arr, x);
+	}
+}
+
+
 
