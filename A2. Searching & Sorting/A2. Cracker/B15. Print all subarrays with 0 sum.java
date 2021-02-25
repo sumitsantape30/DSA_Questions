0125b80410d917101a1fe3b15c3d6d97e1010393
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-25 21:19:27 +0530

    Create B15. Print all subarrays with 0 sum.java

---
+++ A2. Searching & Sorting/A2. Cracker/B15. Print all subarrays with 0 sum.java
@@ -0,0 +1,14 @@
+Print all subarrays with 0 sum
+Given an array, print all subarrays in the array which has sum 0.
+
+Examples:
+
+Input:  arr = [6, 3, -1, -3, 4, -2, 2,
+             4, 6, -12, -7]
+Output:  
+Subarray found from Index 2 to 4
+Subarray found from Index 2 to 6          
+Subarray found from Index 5 to 6
+Subarray found from Index 6 to 9
+Subarray found from Index 0 to 10
+  
