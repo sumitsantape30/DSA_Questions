ce26f761cce1be67d050bebd16375ede36fe5709
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-25 17:59:30 +0530

    Update B12. maximum sum such that no 2 elements are adjacent.java

--- A2. Searching & Sorting/A2. Cracker/B12. maximum sum such that no 2 elements are adjacent.java
+++ A2. Searching & Sorting/A2. Cracker/B12. maximum sum such that no 2 elements are adjacent.java
@@ -15,3 +15,37 @@ a[] = {1,2,3}
 Output: 4
 
 Code:
+package CrackerSandS;
+
+public class Sumsuchthatno2eleareAdjacent {
+	
+	public static int solve( int arr[], int i) {
+		
+		if( i <= -1) {
+			return 0; //agar hum array ke bahar ho chuke hai to wahase kuch mila nhi hume so return 0
+		}
+		
+		if( dp[i] != -1) {
+			
+		}
+		
+		//option 1
+		int op1= arr[i]+ solve(arr, i-1); //first option mai woh element include kiye and do bar jum karwaye coz next element lena nhi hai isliye iske aage bhejdo
+		int op2= solve(arr, i-1); // hum current element include nhi karre to usko aage shift kar denge matlab piche bhejdo coz maine piche se start kiya
+		
+		//in dono mese jo max hoga woh answer hai
+		return Math.max(op1,  op2);
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+
+		int dp[]= {5,5,10,100,10,5};
+		int n= dp.length;
+		solve(n-1, dp); //hum pichese dekhte aare
+		
+	}
+
+}
