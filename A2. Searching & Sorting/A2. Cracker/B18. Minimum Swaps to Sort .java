fb86878de5550494bdd19cd5b2fcff37ec71225d
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-27 22:51:03 +0530

    Update B18. Minimum Swaps to Sort .java

--- A2. Searching & Sorting/A2. Cracker/B18. Minimum Swaps to Sort .java
+++ A2. Searching & Sorting/A2. Cracker/B18. Minimum Swaps to Sort .java
@@ -17,3 +17,91 @@ Output:
 2
 Explaination:
 swap 10 with 3 and swap 19 with 5.
+
+//============================================================== Approach 1=====================================================
+  
+//============================================================== Approach 2 (Using HashMap)======================================
+  package CrackerSandS;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.Arrays;
+import java.util.HashMap;
+
+
+public class MinimumNumberOfSwaps {
+	
+	public static int[] takeInput() {
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		int arr[]= new int[n];
+		
+		for( int i=0; i< arr.length; i++) {
+			arr[i]= s.nextInt();
+		}
+		return arr;
+	}
+	
+	public static int minSwaps(int arr[]) {
+		
+		int n= arr.length;
+		
+		//arraylist banayenge and will use pairs jisme first integer will be element and second integer will be index of the first integer
+		Vector<Pair<Integer,Integer>> v(n);
+		
+		
+	}
+	
+	public static int minSwaps2(int arr[]) {
+		int n= arr.length;
+		int ans=0;
+		int temp[]= Arrays.copyOfRange(arr,0, n);
+		
+		HashMap<Integer,Integer> h= new HashMap<Integer,Integer>(); //thid hashmap stores the index of input array
+		Arrays.sort(arr);
+		
+		for( int i=0; i< arr.length; i++) {
+			h.put(arr[i], i);
+		}
+		
+		for( int i=0; i< arr.length; i++) {
+			
+			if( arr[i] != temp[i]) { //checking if current element is at right place or not
+				
+				ans++;
+				int init= arr[i];
+				
+				swap(arr, i, h.get(temp[i]));
+				
+				h.put( init , h.get(temp[i]));
+				h.put(temp[i], 1);
+			}
+		}
+		return ans;
+	}
+	
+	public static void swap( int arr[], int i, int j) {
+		
+		int temp= arr[i];
+		arr[i]= arr[j];
+		arr[j]= temp;
+	}
+	
+	public static void print(int arr[]) {
+		
+		for( int i=0; i< arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+	
+
+	public static void main(String[] args) {
+
+		int arr[]=  { 101, 758, 315, 730, 47, 619, 460, 479 };
+		int ans= minSwaps2(arr);
+		System.out.println(ans);
+		
+	}
+
+}
