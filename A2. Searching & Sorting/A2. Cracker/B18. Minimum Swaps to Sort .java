0048c2c494fc4609eb991ae275d690065fba1fa7
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-27 18:56:58 +0530

    Create B18. Minimum Swaps to Sort .java

---
+++ A2. Searching & Sorting/A2. Cracker/B18. Minimum Swaps to Sort .java
@@ -0,0 +1,19 @@
+Given an array of n distinct elements. Find the minimum number of swaps required to sort the array in strictly increasing order.
+
+
+Example 1:
+
+Input:
+nums = {2, 8, 5, 4}
+Output:
+1
+Explaination:
+swap 8 with 4.
+Example 2:
+
+Input:
+nums = {10, 19, 6, 3, 5}
+Output:
+2
+Explaination:
+swap 10 with 3 and swap 19 with 5.
