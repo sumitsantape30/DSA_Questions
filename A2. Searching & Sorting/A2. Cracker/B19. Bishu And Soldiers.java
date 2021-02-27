a0541ffc0af9d6a217bd761bf1e23c4ab17aba6d
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-27 23:02:13 +0530

    Create B19. Bishu And Soldiers.java

---
+++ A2. Searching & Sorting/A2. Cracker/B19. Bishu And Soldiers.java
@@ -0,0 +1,18 @@
+Problem
+Bishu went to fight for Coding Club. There were N soldiers with various powers. 
+  There will be Q rounds to fight and in each round Bishu's power will be varied. With power M, Bishu can kill all the soldiers
+  whose power is less than or equal to M(<=M). After each round, All the soldiers who are dead in previous round will reborn.
+  Such that in each round there will be N soldiers to fight. As Bishu is weak in mathematics, help him to count the number of soldiers that
+  he can kill in each round and total sum of their powers.
+ 
+Sample Input
+7
+1 2 3 4 5 6 7
+3
+3
+10
+2
+Sample Output
+3 6
+7 28
+2 3
