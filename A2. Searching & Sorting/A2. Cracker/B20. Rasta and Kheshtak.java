2a8ce97e5da643f57f218388c2f501cb133d583e
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-28 16:19:44 +0530

    Update and rename B20. to B20. Rasta and Kheshtak.java

---
+++ A2. Searching & Sorting/A2. Cracker/B20. Rasta and Kheshtak.java
@@ -0,0 +1,28 @@
+Rasta and Kheshtak
+
+Problem
+See Russian translation
+
+Rasta is a big fan of Kheshtaks. A Kheshtak is a rectangle that in each of it cells there is an integer.
+
+Today rasta came up with an interesting problem, Biggest Common Subsquare (BCS). A Kheshtak is called a Square if the number of its columns is equal to the number of its rows. A Square S is called a subsqaue of a Kheshtak A if and only if we can turn A to S by deleting some of its rows and some of its columns (maybe none).
+
+He gives you two Kheshtaks, A and B (A one is n × m and B is x × y).
+  
+Output format
+Print the size of BCS of A and B in a single line (size of a Square is number of its rows).
+
+Sample Input
+3 3
+1 2 0
+1 2 1
+1 2 3
+3 3
+0 1 2
+1 1 2
+3 1 2
+Sample Output
+2
+
+Code:
+
