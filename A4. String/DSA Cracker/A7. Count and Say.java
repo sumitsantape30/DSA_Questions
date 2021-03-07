9586d352bcf08a8bbe008c02bb24c53817113fe5
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-08 00:25:00 +0530

    Update A7. Count and Say.java

--- A4. String/DSA Cracker/A7. Count and Say.java
+++ A4. String/DSA Cracker/A7. Count and Say.java
@@ -28,3 +28,5 @@ countAndSay(1) = "1"
 countAndSay(2) = say "1" = one 1 = "11"
 countAndSay(3) = say "11" = two 1's = "21"
 countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+
+ Code:
